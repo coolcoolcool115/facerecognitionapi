a49e9356eb6c7bcfc63c1a88bc90c5c3ffd54a44
jordon ogonoski <coolcoolcool115@gmail.com>
2020-05-15 18:26:39 -0400

    fixing port number

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.get('/profile/:id', profile.handleProfileGet(db))
 app.put('/image', image.handleImage(db))
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(3000, () => {
-	console.log('app is running');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 });
